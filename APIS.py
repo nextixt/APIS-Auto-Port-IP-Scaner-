dfbf343ea05b14947517bd647dc0e3c9180f7eab
Nextixt <nextixt@gmail.com>
2026-03-08 21:03:31 +0300

    Update APIS.py

--- APIS.py
+++ APIS.py
@@ -41,6 +41,11 @@ try:
 except KeyboardInterrupt:
         print("\n Exiting Program...")
         sys.exit()
+              
+        
+except KeyboardInterrupt:
+        print("\n Exiting Program...")
+        sys.exit()
 except socket.gaierror:
         print("\n Hostname Could Not Be Resolved!")
         sys.exit()

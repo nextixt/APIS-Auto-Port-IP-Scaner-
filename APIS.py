290939d6a4ff800c1b6fa5a73b7f762a0176563a
Nextixt <nextixt@gmail.com>
2026-03-08 20:51:44 +0300

    Create APIS.py

---
+++ APIS.py
@@ -0,0 +1,46 @@
+import pyfiglet
+import sys
+import socket
+ 
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
+ 
+# Defining a target
+if len(sys.argv):
+    target_ip = sys.argv[2]
+    mode = sys.argv[1]
+else:
+    print("Invalid amount of Argument")
+
+try:
+    if mode == "SINGLE":
+        target_port = int(sys.argv[3])
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target_ip,target_port))
+        if result == 0:
+                print(f"Port {target_port} is open")
+                s.close()
+        else:
+            print(f"Port {target_port} is closed")
+
+    elif mode == "RANGE":
+        start = int(sys.argv[3])
+        stop = int(sys.argv[4])
+        for port in range(start, stop + 1):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            result = s.connect_ex((target_ip,port))
+            if result == 0:
+                print(f"Port {port} is open")
+                s.close()
+            else:
+                print(f"Port {port} is closed")
+              
+        
+except KeyboardInterrupt:
+        print("\n Exiting Program...")
+        sys.exit()
+except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved!")
+        sys.exit()
